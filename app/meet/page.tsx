f2ad8e997b6f3b13e7c74c27df1ffccfaabc2c18
Michael Ferreira <michaeljf.2007@gmail.com>
2026-03-11 09:37:03 -0400

    feat: add google calendar integration

---
+++ app/meet/page.tsx
@@ -0,0 +1,11 @@
+export default function MeetingsPage() {
+    return (
+        <div>
+            <h1>Meetings Page</h1>
+            <p>
+                Welcome to the meetings page. Here you can find all your
+                scheduled meetings and manage them.
+            </p>
+        </div>
+    );
+}
